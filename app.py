ae673d760c1b1bd3a8e4c2cc0d159cb96bdf8749
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-24 15:27:59 -0400

    routes

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def main_route():
     return "<p>Hello, World!</p>"
 
 @app.route("/twitter")
-def hello_world():
+def twitter_route():
     return "buyreal.tech ahahaha put twitter next steps here"
