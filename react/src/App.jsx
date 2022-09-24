45f6b25d3b5bd16b1cf7ddcced30a487c8dca4b1
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-24 15:53:33 -0400

    refactor

---
+++ react/src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { usePlaidLink } from "react-plaid-link";
+import "./App.scss";
+
+function App(props) {
+  const [token, setToken] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const onSuccess = useCallback(async (publicToken) => {
+    setLoading(true);
+    await fetch("/api/set_access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: `public_token=${publicToken}`,
+    });
+  }, []);
+
+  // Creates a Link token
+  const createLinkToken = React.useCallback(async () => {
+    // For OAuth, use previously generated Link token
+    if (window.location.href.includes("?oauth_state_id=")) {
+      const linkToken = localStorage.getItem('link_token');
+      setToken(linkToken);
+    } else {
+      const response = await fetch("/api/create_link_token", {});
+      const data = await response.json();
+      setToken(data.link_token);
+      localStorage.setItem("link_token", data.link_token);
+    }
+  }, [setToken]);
+
+  // Fetch transaction data
+  const getTransactions = React.useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("/api/transactions", {});
+    const data = await response.json();
+    setData(data);
+    setLoading(false);
+  }, [setData, setLoading]);
+
+  let isOauth = false;
+
+  const config = {
+    token,
+    onSuccess,
+  };
+
+  // For OAuth, configure the received redirect URI
+  if (window.location.href.includes("?oauth_state_id=")) {
+    config.receivedRedirectUri = window.location.href;
+    isOauth = true;
+  }
+  const { open, ready } = usePlaidLink(config);
+
+  useEffect(() => {
+    if (token == null) {
+      createLinkToken();
+    }
+    if (isOauth && ready) {
+      open();
+    }
+  }, [token, isOauth, ready, open]);
+  
+  return (
+    <div>
+      <button onClick={() => open()
+        } disabled={!ready}>
+        <strong>Link account</strong>
+      </button>
+      <button onClick={() => getTransactions()
+        } disabled={!ready}> Get Transactions 
+      </button>
+      {!loading &&
+        data != null &&
+        Object.entries(data).map((entry, i) => (
+          <pre key={i}>
+            <code>{JSON.stringify(entry[1], null, 2)}</code>
+          </pre>
+        )
+      )}
+    </div>
+  );
+}
+
+export default App;
