6eff4022837641f3fe0e10c3b5b0818dfc374845
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-24 12:36:53 -0400

    twitter

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/twitter")
+def hello_world():
+    return "buyreal.tech ahahaha put twitter next steps here"
