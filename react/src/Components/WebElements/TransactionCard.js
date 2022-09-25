d439ed297d4bf5f6f2940fb4bfddb564fda246d1
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-25 00:00:58 -0400

    requirements.txt

--- react/src/Components/WebElements/TransactionCard.js
+++ react/src/Components/WebElements/TransactionCard.js
@@ -8,6 +8,22 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 
 export default function TransactionCard({ img }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <>
       <Card sx={{ maxWidth: 345 }}>
