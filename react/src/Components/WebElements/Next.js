7f55e104595e68316142cdf731c7137c2f676852
Dylan Scott <dylanscott821@gmail.com>
2022-09-25 00:55:05 -0400

    Update Text

--- react/src/Components/WebElements/Next.js
+++ react/src/Components/WebElements/Next.js
@@ -5,11 +5,13 @@ import TextContainer from "../TextContainer";
 
 function Next({}) {
   const title="Next Steps"
-  const body=["Moving BuyReal to Twitter! The random purchases will be tweeted directly through each user’s account, allowing everyone on Twitter to not only view these purchases, but to also reply, retweet and like", 
-  "Incorporating three never-before-seen BuyReal modes: ",
-  " - BeReal Mode: BuyReal will tweet a user's random transaction once a day",
-  " - Honest Mode: BuyReal will tweet all of the transactions of the user ",
-  " - Charity Mode: BuyReal will only tweet charity donations"]
+  const body=["User Accounts. Currently, only a single credit card can be linked across the entire service so it is only functional as a proof of concept. By adding user accounts and linking Plaid to our MongoDB database, people could create BuyReal accounts and add as many cards as they want!", 
+  "Moving BuyReal to Twitter! The random purchases will be tweeted directly through each user’s account, allowing everyone on Twitter to not only view these purchases but to also reply, retweet, and like. ",
+  "Incorporating three BuyReal modes: ",
+  "- BeReal Mode: BuyReal will tweet a user's random transaction once a day",
+  "- Honest Mode: BuyReal will tweet all of the transactions of the user ",
+  "- Charity Mode: BuyReal will only tweet charity donations",
+  "Real Data. Once implementing more of the other features, we could move from the Plaid Sandbox instance to the Plaid Developer or Plaid Production instance so we can pull real transaction history from users."]
 
   return (
     <>
--- react/src/Components/WebElements/Problem.js
+++ react/src/Components/WebElements/Problem.js
@@ -6,9 +6,9 @@ import TextContainer from "../TextContainer";
 
 function Problem({ }) {
   const title = "Problem"
-  const body = ["Many people make irresponsible and inconsiderate financial decisions because of the freedom that comes with a credit card. We wanted to create a project that would allow people to manage their purchases, as these purchases would become public to other BuyReal users– creating a community of people trying to get their spending on track. The website can also just be fun to use. It;s funny and exciting to see a random purchase you made that day online!",
-    "BuyReal can help influencers advance their careers as well, since companies can reach out to them asking to make purchases, as a form of advertisement. Either way, influencer or not, BuyTech serves as free advertisement for various brands that will inevitably grow popular just from people seeing purchases from certain influencers, stores and brands on BuyReal.tech",
-    "Finally, BuyReal could be used to hold celebrities accountable for their financial actions. It is time for us to see just how much Taylor is paying for her daily commute, or if other rich celebrities actually give to charity. Seeing others donate to charity may even influence society as a whole to donate more!"
+  const body = ["Many people make irresponsible and inconsiderate financial decisions because of the freedom that comes with a credit card. We wanted to create a project that would allow people to manage their purchases, as these purchases would become public to other BuyReal users — creating a community of people trying to get their spending on track. The website can also just be fun to use. It is funny and exciting to see a random purchase you made that day online!", 
+  "BuyReal can also help influencers advance their careers, since companies can reach out to them asking to make purchases, as a form of advertisement. Either way, influencer or not, BuyReal serves as free advertisement for various brands that will inevitably grow popular just from people seeing purchases from certain influencers, stores, and brands on BuyReal.tech.",
+  "Finally, BuyReal could be used to hold celebrities accountable for their financial actions. It is time for us to see just how much Taylor is paying for her daily commute, or if other rich celebrities actually give to charity. Seeing others donate to charity may even influence society as a whole to donate more!"
   ]
   return (
     <Grid id={'Problem'} container spacing={2} >
